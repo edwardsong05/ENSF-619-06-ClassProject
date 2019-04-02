035b9b802deb5959b2bd70b8a556eb355de99693
Eiden Yoshida <eiden.yoshida@ucalgary.ca>
2019-04-02 09:06:23 -0600

    Added basic league and team read APIs

--- FantasyHockeySite/FantasyHockeySite/urls.py
+++ FantasyHockeySite/FantasyHockeySite/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('fantasy/', include('fantasy.urls')),
     path('admin/', admin.site.urls),
+    re_path('api/(?P<version>(v1|v2))/', include('apis.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
